9278ff32743892c1447072ebd1dd9411d53fc3b8
Andrew Tolbert <andrew.tolbert@datastax.com>
2017-04-17 13:28:30 -0500

    Add integration test phase
    Any class named *IntegrationTest.java is considered an integration test.
    
    Also added initial integration test to verify peer priming is correct
    when using java driver.

---
+++ native-server/src/test/java/com/datastax/simulacron/server/PeerMetadataIntegrationTest.java
@@ -0,0 +1,76 @@
+package com.datastax.simulacron.server;
+
+import com.datastax.driver.core.Host;
+import com.datastax.driver.core.NettyOptions;
+import com.datastax.driver.core.Session;
+import com.datastax.simulacron.common.cluster.Cluster;
+import com.datastax.simulacron.common.cluster.DataCenter;
+import com.datastax.simulacron.common.cluster.Node;
+import io.netty.channel.EventLoopGroup;
+import org.junit.Test;
+
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PeerMetadataIntegrationTest {
+
+  private final Server server = Server.builder().build();
+
+  /**
+   * A custom {@link NettyOptions} that shuts down the {@link EventLoopGroup} after no quiet time.
+   */
+  private static NettyOptions nonQuietClusterCloseOptions =
+      new NettyOptions() {
+        @Override
+        public void onClusterClose(EventLoopGroup eventLoopGroup) {
+          eventLoopGroup.shutdownGracefully(0, 15, SECONDS).syncUninterruptibly();
+        }
+      };
+
+  private static com.datastax.driver.core.Cluster.Builder defaultBuilder() {
+    return com.datastax.driver.core.Cluster.builder().withNettyOptions(nonQuietClusterCloseOptions);
+  }
+
+  @Test
+  public void testClusterDiscovery() throws Exception {
+    // Validate that peers as appropriately discovered when connecting to a node.
+    Cluster cluster = Cluster.builder().withNodes(3, 3, 3).build();
+    Cluster boundCluster = server.register(cluster).get(5, TimeUnit.SECONDS);
+
+    DataCenter dc0 = boundCluster.getDataCenters().iterator().next();
+    Node node0 = dc0.getNodes().iterator().next();
+
+    try (com.datastax.driver.core.Cluster driverCluster =
+        defaultBuilder()
+            .addContactPointsWithPorts((InetSocketAddress) node0.getAddress())
+            .build()) {
+      driverCluster.init();
+
+      // Should be 9 hosts
+      assertThat(driverCluster.getMetadata().getAllHosts()).hasSize(9);
+
+      // Connect and ensure pools are created to local dc hosts.
+      Session session = driverCluster.connect();
+
+      // Verify hosts connected to are only those in the local DC.
+      Collection<SocketAddress> connectedHosts =
+          session
+              .getState()
+              .getConnectedHosts()
+              .stream()
+              .map(Host::getSocketAddress)
+              .collect(Collectors.toList());
+
+      Collection<SocketAddress> dcHosts =
+          dc0.getNodes().stream().map(Node::getAddress).collect(Collectors.toList());
+
+      assertThat(connectedHosts).hasSameElementsAs(dcHosts);
+    }
+  }
+}
