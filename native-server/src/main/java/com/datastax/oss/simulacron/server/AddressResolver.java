d1f4c4e97832f26084df1cd41871f1532b71ac05
Andrew Tolbert <andrew.tolbert@datastax.com>
2017-07-24 21:22:06 -0500

    Warn if any of first 100 IP addresses provided aren't usable on OS X
    Also provide a link on how to remedy.

--- native-server/src/main/java/com/datastax/oss/simulacron/server/AddressResolver.java
+++ native-server/src/main/java/com/datastax/oss/simulacron/server/AddressResolver.java
@@ -16,15 +16,20 @@
 package com.datastax.oss.simulacron.server;
 
 import io.netty.channel.local.LocalAddress;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.net.SocketAddress;
 import java.net.UnknownHostException;
 import java.util.Queue;
 import java.util.UUID;
 import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public interface AddressResolver extends Supplier<SocketAddress> {
 
@@ -43,16 +48,13 @@ public interface AddressResolver extends Supplier<SocketAddress> {
    * A resolver that returns next unused IP address at port 9042. Starts from an input address and
    * cycles up to the next subnet as it goes, i.e. the order would go something like:
    *
-   * <p>
-   *
-   * <p>
-   *
-   * <p>
-   *
    * <p>127.0.0.1, 127.0.0.2 ... 127.0.0.255, 127.0.1.1, 127.0.1.2 and so on.
    */
   class Inet4Resolver implements AddressResolver {
+
+    private static final Logger logger = LoggerFactory.getLogger(Inet4Resolver.class);
     private final AtomicReference<byte[]> ipParts;
+    private static final AtomicBoolean WARNED = new AtomicBoolean();
     private final int port;
 
     // Use priority queue so released addresses are sorted by ip address.
@@ -98,6 +100,66 @@ public interface AddressResolver extends Supplier<SocketAddress> {
       System.arraycopy(startingAddress, 0, ipAddr, 0, 4);
       this.ipParts = new AtomicReference<>(ipAddr);
       this.port = port;
+      checkAddressPresence();
+    }
+
+    private void checkAddressPresence() {
+      if (WARNED.get()) {
+        return;
+      }
+
+      // checks that the OS has 100 local ip addresses.
+      // this check is really only needed for OS X, otherwise return.
+      String osName = System.getProperty("os.name", "none");
+      if (!osName.toLowerCase().startsWith("mac")) {
+        return;
+      }
+      byte[] ipBytes = ipParts.get();
+
+      for (int i = 0; i < 100; i++) {
+        InetAddress inetAddress = inetAddress(ipBytes);
+        try {
+          // Attempt to create socket on that address.
+          new ServerSocket(0, 0, inetAddress);
+        } catch (IOException e) {
+          if (WARNED.compareAndSet(false, true)) {
+            logger.warn(
+                "Detected that {} is not available for use.  "
+                    + "Refer to https://goo.gl/Ru7gUj for information on how to provision IPs on OS X.",
+                inetAddress);
+          }
+          return;
+        }
+        ipBytes = nextAddressBytes(ipBytes);
+      }
+    }
+
+    private InetAddress inetAddress(byte[] ipBytes) {
+      byte[] ipAddr = new byte[4];
+      System.arraycopy(ipBytes, 0, ipAddr, 0, 4);
+
+      InetAddress addr;
+      try {
+        addr = InetAddress.getByAddress(ipAddr);
+      } catch (UnknownHostException uhe) {
+        throw new IllegalArgumentException("Could not create ip address", uhe);
+      }
+      return addr;
+    }
+
+    private byte[] nextAddressBytes(byte[] currentIpBytes) {
+      byte[] newBytes = new byte[4];
+      System.arraycopy(currentIpBytes, 0, newBytes, 0, 4);
+      for (int i = currentIpBytes.length - 1; i > 0; i--) {
+        // roll over ipAddress if we max out the current octet (255)
+        if (newBytes[i] == (byte) 0xFE) {
+          newBytes[i] = 0;
+        } else {
+          ++newBytes[i];
+          break;
+        }
+      }
+      return newBytes;
     }
 
     @Override
@@ -110,27 +172,11 @@ public interface AddressResolver extends Supplier<SocketAddress> {
         // get current address and increment to create next one.
         // if CAS fails, try again until it works.
         while (true) {
-          byte[] ipAddr = new byte[4];
           byte[] ref = ipParts.get();
-          System.arraycopy(ref, 0, ipAddr, 0, 4);
+          InetAddress addr = inetAddress(ref);
+          byte[] next = nextAddressBytes(ref);
 
-          InetAddress addr = null;
-          try {
-            addr = InetAddress.getByAddress(ipAddr);
-          } catch (UnknownHostException uhe) {
-            throw new IllegalArgumentException("Could not create ip address", uhe);
-          }
-
-          for (int i = ipAddr.length - 1; i > 0; i--) {
-            // roll over ipAddress if we max out the current octet (255)
-            if (ipAddr[i] == (byte) 0xFE) {
-              ipAddr[i] = 0;
-            } else {
-              ++ipAddr[i];
-              break;
-            }
-          }
-          if (ipParts.compareAndSet(ref, ipAddr)) {
+          if (ipParts.compareAndSet(ref, next)) {
             return new InetSocketAddress(addr, this.port);
           }
         }
